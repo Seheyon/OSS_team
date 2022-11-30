6a1b9d8eafaeca40179e31a05ed87306b81dc795
Sehyeon <104759146+Seheyon@users.noreply.github.com>
2022-11-30 21:07:43 +0900

    Update testmenu.js

--- testmenu.js
+++ testmenu.js
@@ -1,18 +1,22 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const today(rtm, channel) {
+  const axios = require('axios');
+  const cheerio = require('cheerio');
 
-async function webScraping(url, selector) {
-  let res = [];
-  let html = await axios.get(url);
-  let $ = cheerio.load(html.data);
+  async function webScraping(url, selector) {
+    let res = [];
+    let html = await axios.get(url);
+    let $ = cheerio.load(html.data);
   
-  for(let v of $(selector)) {   
-    res.push($(v).text());
+    for(let v of $(selector)) {   
+      res.push($(v).text());
+    }
+    
+    return res;
   }
-  
-  return res;
+
+  webScraping('https://dev.epiloum.net', 'h2.entry-title').then((res) => {
+    console.log(res)
+  });
 }
 
-webScraping('https://dev.epiloum.net', 'h2.entry-title').then((res) => {
-  console.log(res)
-});
+module.exports = todaymenu;
