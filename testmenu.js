7d171c00ff0212370c96f5a2aa16f422dbeb68ad
Sehyeon <104759146+Seheyon@users.noreply.github.com>
2022-11-30 21:03:41 +0900

    Create testmenu.js

---
+++ testmenu.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function webScraping(url, selector) {
+  let res = [];
+  let html = await axios.get(url);
+  let $ = cheerio.load(html.data);
+  
+  for(let v of $(selector)) {   
+    res.push($(v).text());
+  }
+  
+  return res;
+}
+
+webScraping('https://dev.epiloum.net', 'h2.entry-title').then((res) => {
+  console.log(res)
+});
